75fdf235d04668e7e207fffc2b4e47db1f332fcd
jovitamaharani <jovitamaharani321@gmail.com>
2024-01-25 10:48:20 +0700

    baruuu

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,8 +7,6 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
-
 
 class RegisterController extends Controller
 {
@@ -52,14 +50,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'ends_with:@gmail.com'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ],[
-            'name.required' => 'Nama harus di inputkan',
-            'email.required' => 'Email harus di inputkan',
-            'email.ends_with' => 'Email harus berupa @gmail.com',
-            'password.required' => 'Password harus di inputkan',
-            'password.min' => 'Password harus :min digit',
         ]);
     }
 
