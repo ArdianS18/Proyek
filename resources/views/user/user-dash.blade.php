fd61bd91d2cd861a2098a909d2b7416ba001838c
ArdianS18 <ardiansupriadi464@gmail.com>
2024-01-26 13:38:46 +0700

    baru untuk user tiket

--- resources/views/user/user-dash.blade.php
+++ resources/views/user/user-dash.blade.php
@@ -143,22 +143,21 @@
               <p><strong>Harga Tiket Dewasa:</strong> Rp {{ $destinasi->tiket_dewasa }}</p>
               <p><strong>Harga Tiket Dewasa:</strong> Rp {{ $destinasi->tiket_dewasa }}</p> --}}
                <!-- Modal toggle -->
-              <button data-modal-target="tambahdata" data-modal-toggle="tambahdata" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
+              <button data-modal-target="tambahdata{{$destinasi->id}}" data-modal-toggle="tambahdata{{$destinasi->id}}" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
                 Pesan Tiket
               </button>
             </div>
         </a>
     @endif
-@endforeach
 
-@if (isset($_GET['search']) && !empty($_GET['search']) && $destinasis->where('wisata', 'like', '%' . $_GET['search'] . '%')->count() === 0)
+    @if (isset($_GET['search']) && !empty($_GET['search']) && $destinasis->where('wisata', 'like', '%' . $_GET['search'] . '%')->count() === 0)
     <p>Data tidak ditemukan.</p>
 @endif
 
 
 
 <!-- Main modal -->
-<div id="tambahdata" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="tambahdata{{$destinasi->id}}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
   <div class="relative p-4 w-full max-w-md max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -180,11 +179,9 @@
 
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
-                    @foreach ($destinasis as $key => $destinasi)
                     <label for="">Wisata : <p></p></label>
                     <input type="hidden" name="destinasi_id" id="" class="form-control  @error('destinasi_id') is invalid @enderror" value="{{$destinasi->id}}">
                         <input type="text" id="destinasi_id" name="" class="form-control @error('destinasi_id') is-invalid @enderror" value="{{ $destinasi->wisata }}"  readonly>
-                    @endforeach
                     </div>
 
                 <div class="col-span-2">
@@ -229,6 +226,7 @@
   </div>
 </div>
 
+@endforeach
 
 
 </body>
