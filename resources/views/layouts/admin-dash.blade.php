547be4b1251e009c173a88b178b64969daad95c9
jovitamaharani <jovitamaharani321@gmail.com>
2024-01-31 14:47:59 +0700

    galeri admin

--- resources/views/layouts/admin-dash.blade.php
+++ resources/views/layouts/admin-dash.blade.php
@@ -202,6 +202,14 @@
                 <span class="flex-1 ms-3 whitespace-nowrap">Pembayaran</span>
             </a>
         </li>
+        <li>
+            <a href="/galeri" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('galeri.index') ? 'active' : '' }}">
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.3 6m2.3-9h0M4 19h16c.6 0 1-.4 1-1V6c0-.6-.4-1-1-1H4a1 1 0 0 0-1 1v12c0 .6.4 1 1 1Z"/>
+                  </svg>
+               <span class="flex-1 ms-3 whitespace-nowrap">Galeri</span>
+           </a>
+       </li>
         <li class="flex items-center text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('ulasanadmin.index') ? 'active' : '' }}">
            <a href="/ulasanadmin" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
