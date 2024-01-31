89a08980059293c1fa685de4d501e43878f2e960
jovitamaharani <jovitamaharani321@gmail.com>
2024-01-31 14:28:41 +0700

    btn tiket lagi

--- resources/views/user/tiket-user.blade.php
+++ resources/views/user/tiket-user.blade.php
@@ -47,7 +47,7 @@
         Bayar Sekarang
     </button>
     @elseif ($tiket->terima == "Tidak Diterima")
-      <button data-modal-target="editbayar{{$tiket->id}}" data-modal-toggle="editbayar{{$tiket->id}}" class="text-red-600 border border-red-600 hover:bg-white hover:text-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
+      <button data-modal-target="editbayar{{$tiket->id}}" data-modal-toggle="editbayar{{$tiket->id}}" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" type="button">
           Bayar Ulang Sekarang
       </button>
     @else
